28a63aed53e336e4db2e52f35b7ecf84cd511426
Live session user <a1705111@student.adelaide.edu.au>
2019-09-06 16:41:36 +0930

    Adds comments to supernova mutation

--- SuperNovaMutation.java
+++ SuperNovaMutation.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+// Class for performing a mutation for TSP.
+// This is a composite of the implosion and explosion mutations - 
+// Points in the radius have a 50% chance of being imploded and
+// a 50% chance of being exploded.
 public class SuperNovaMutation implements ITSPMutation
 {
     // Explosion centres will only be placed between (min_x, min_y)
@@ -10,16 +14,25 @@ public class SuperNovaMutation implements ITSPMutation
     private double min_y;
     private double max_y;
     
+    // Explosion radius is between these two values. 
     private double min_nova_size;
     private double max_nova_size;
     
+    // Constructor for supernova mutator.
+    // x_min should be the smallest x value in the instance intended to be mutated.
+    // y_min should be the smallest y value in the instance intended to be mutated.
+    // The max should be the largest x and y value respecively in the instance intended to be mutated.
+    // The min and max nova size is set to be 20% and 60% of the smaller of the range of x values and y values. 
     public SuperNovaMutation(double x_min_value, double y_min_value, double x_max_value, double y_max_value)
     {
+        // Min and max values are used to determine where the nova can occur.
         min_x = x_min_value;
         min_y = y_min_value;
         max_x = x_max_value;
         max_y = y_max_value;
         
+        // The min of difference between x min and y min
+        // Is used to cap the radius of a nova. 
         double x_dist = max_x - min_x;
         double y_dist = max_y - min_y;
         
@@ -40,28 +53,27 @@ public class SuperNovaMutation implements ITSPMutation
         max_nova_size = dist * 0.6;
     }
     
+    // Returns a nova mutated TSP instance based on parent. 
     public TSP_Instance mutate(TSP_Instance parent)
     {
         ArrayList<Point> points = new ArrayList<Point>(parent.getCoordinates());
         ArrayList<Point> result = new ArrayList<Point>();
         Random rand = RandomNumberGenerator.getRandom();
         
-        // Get the centre of the nova.
+        // Generate the centre of the nova.
         double x_center = min_x + (max_x - min_x) * rand.nextDouble();
         double y_center = min_y + (max_y - min_y) * rand.nextDouble();
-        
-        System.out.println(x_center + "," + y_center);
         Point center = new Point(x_center, y_center);
-        center = new Point((min_x + max_x)/2, (min_y + max_y)/2);
+        
         // Get the radius of the nova.
         double radius = min_nova_size + (max_nova_size - min_nova_size) * rand.nextDouble();
        
-        System.out.println(radius);
-        
+        // For each point 
         for (Point point : points)
         {
             double temp = rand.nextDouble();
             
+            // 50% chance of exploding the point, 50% chance of imploding it.
             if (temp < 0.5)
             {
                 point = explode(point, center, radius);
@@ -77,9 +89,13 @@ public class SuperNovaMutation implements ITSPMutation
         return new TSP_Instance(result);
     }
     
+    // Explodes a point within radius of center,
+    // pushing it outside of the circle with center and radius.
     private Point explode(Point target, Point center, double radius)
     {
+        // Get distance from center of explosion.
         double distance = center.distance(target);
+        
         // Ignore points not within the explosion.
         if (distance > radius)
         {
@@ -107,26 +123,35 @@ public class SuperNovaMutation implements ITSPMutation
         return result;
     }
     
+    // Imploads a point within radius, bringing it close to center.
     private Point implode(Point target, Point center, double radius)
     {
+        // Get the distance from the center of the nova.
         double distance = center.distance(target);
+        
+        // Ignore points outside of the radius.
         if (distance > radius)
         {
             return target;
         }
         
+        // Determine the power of the explosion from a normal distribution.
         Random rand = RandomNumberGenerator.getRandom();
         double power = Math.abs(rand.nextGaussian());
         
+        // Performs center - target to get direction vector from center to target. 
         Point result = center.subtract(target);
         
+        // Set power to radius if it is smaller. 
         if (radius < power)
         {
             power = radius;
         }
         
+        // Multiply direction vector by factor to get dist from center
         result.scalar_factor(power);
         
+        // Add the center to the direction vector to get the final point.
         result = result.add(target);
         
         return target;
