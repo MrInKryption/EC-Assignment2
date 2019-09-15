335633bde7ed341489ad47eaf75113f31cb09890
Live session user <a1705111@student.adelaide.edu.au>
2019-09-15 17:09:21 +0930

    Adds twoOpt fitness function for TSP

--- ITSPFitnessFunction.java
+++ ITSPFitnessFunction.java
@@ -1,3 +1,5 @@
+// Interface for classes which calculate the fitness of TSP_Instances,
+// allowing for them to be interchanged. 
 public interface ITSPFitnessFunction
 {
     public double fitness(TSP_Instance instance);
