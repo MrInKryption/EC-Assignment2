bc5f170371b1d089ec0c9650d6eddf5ac67cc6b3
protofall <david74437@gmail.com>
2019-09-21 21:35:32 +0930

    This stinks

---
+++ EvolutionaryAlgorithm2.java
@@ -0,0 +1,138 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+// Evolutionary Algorithm class
+public class EvolutionaryAlgorithm2 implements IEvolutionary
+{
+    public EvolutionaryAlgorithm2()
+    {
+    }
+
+    // Evolutionary Algorithm to produce TSP Instances maximising
+    public void EvolutionaryAlgorithm(TSP_Instance tsp)
+    {
+        TSP_Instance.setFitnessFunction(new TwoOptFitnessFunction());
+
+        // Create copy of initial TSP Instance as well as create
+        // a variable to store the child TSP Instance after mutation.
+        TSP_Instance currentTsp = new TSP_Instance(tsp.getCoordinates());
+        TSP_Instance childTsp;
+
+        // Calculate fitness of the current TSP Instance as well as create
+        // a variable to store the fitness of any children.
+        double currentFitness = currentTsp.getFitness();
+        double childFitness;
+
+        //THINGS
+        int choice;
+        int option;
+        int num_cities = tsp.getCoordinates().size();
+        // Point p = new Point();
+        Point p;
+        double city_dim;
+        long ave;
+
+        // The number of times we wish to run the program
+        int runs = 10000;
+
+        //DEBUG
+        int debug1 = 0;
+        int debug2 = 0;
+
+        // Initialise random object.
+        Random rand = RandomNumberGenerator.getRandom();
+
+        // Loop until stop condition is met (runs == 10000).
+        while (runs > 0)
+        {
+            runs--;
+
+            // Reset vars incase somehow we don't find a suitable city
+            choice = 0;
+            ave = 0;
+
+            // Choose which kind of city we want to find
+            option = rand.nextInt(4);   //We only want 4 options, [0...3]
+
+            //Set the initial check value
+            if(option == 0 || option == 1){
+                city_dim = currentTsp.getCoordinates().get(0).getX();
+            }
+            else{
+                city_dim = currentTsp.getCoordinates().get(0).getY();
+            }
+
+            //Using the random option, find the city with the largest or smallest x/y
+            for(int i = 0; i < num_cities; i++){
+                p = currentTsp.getCoordinates().get(i);
+                switch(option){
+                    case 0: //Finding the individual with the lowest X
+                        if(p.getX() < city_dim){
+                            city_dim = p.getX();
+                            choice = i;
+                        }
+                        ave += p.getX();
+                        break;
+                    case 1: //highest X
+                        if(p.getX() > city_dim){
+                            city_dim = p.getX();
+                            choice = i;
+                        }
+                        ave += p.getX();
+                        break;
+                    case 2: //lowest Y
+                        if(p.getY() < city_dim){
+                            city_dim = p.getY();
+                            choice = i;
+                        }
+                        ave += p.getY();
+                        break;
+                    case 3: // highest Y
+                        if(p.getY() > city_dim){
+                            city_dim = p.getY();
+                            choice = i;
+                        }
+                        ave += p.getY();
+                        break;
+                    default:
+                        System.out.println("How'd you get here? Value = " + option);
+                }
+            }
+            ave /= num_cities;
+
+            // Copy current TSP Instance for mutation.
+            childTsp = new TSP_Instance(currentTsp.getCoordinates());
+
+            //Mutate the child by DOING SOMETHING to it
+            if(option == 0 || option == 1){
+                childTsp.getCoordinates().get(choice).setX(ave);
+            }
+            else{
+                childTsp.getCoordinates().get(choice).setY(ave);
+            }
+
+            // Calculate the childs fitness and compare against the current parents fitness.
+            // Choose the TSP Instance with the best fitness.
+            childFitness = childTsp.getFitness();
+            if (childFitness > currentFitness)
+            {
+                currentTsp = childTsp;
+                currentFitness = childFitness;
+                debug1++;
+            }
+            else if (childFitness < currentFitness){
+                debug2++;
+            }
+
+        }
+        System.out.println("DEBUG VALUES (Replaces VS Unchanged): " + debug1 + ":" + debug2);
+        System.out.println("Final fitness: " + currentFitness);
+    }
+
+    // private Point mutate(Point p1, Point p2, boolean method)
+    // {
+    //     double temp = x;
+    //     x = y;
+    //     y = temp;
+    // }
+}
--- Main.java
+++ Main.java
@@ -16,48 +16,51 @@ public class Main
             tsp = new TSP_Instance(10, 0, 10);
             EvolutionaryAlgorithm1 ea = new EvolutionaryAlgorithm1();
             //ea.EvolutionaryAlgorithm(tsp);
-            
-            TSPEvolutionaryAlgorithm alg = new TSPEvolutionaryAlgorithm(0.05, 4, 1000);
-            TSPEvolutionaryAlgorithm algB = new TSPEvolutionaryAlgorithm(0.05, 4, 1000);
-            TSPEvolutionaryAlgorithm algC = new TSPEvolutionaryAlgorithm(0.05, 4, 1000);
-            ArrayList<TSP_Instance> population = new ArrayList<TSP_Instance>();
-            ArrayList<TSP_Instance> populationB = new ArrayList<TSP_Instance>();
-            ArrayList<TSP_Instance> populationC = new ArrayList<TSP_Instance>();
-            int population_size = 10;
-            int instance_size = 100;
-            for (int i = 0; i < population_size; i++)
-            {
-                population.add(new TSP_Instance(instance_size, 0, 100));
-                populationB.add(new TSP_Instance(instance_size, 0, 100));
-                populationC.add(new TSP_Instance(instance_size, 0, 100));
-            }
-            
-            TwoOptFitnessFunction fitness = new TwoOptFitnessFunction();
-            FastNovaMutation mutator = new FastNovaMutation(3);
-            SuperNovaMutation superMutator = new SuperNovaMutation(0, 0, 100, 100);
-            OffsetMutation offsetMutator = new OffsetMutation();
-            MeanTSPCrossover meanCrossover = new MeanTSPCrossover();
-            SchoolyardTeamsTSPCrossover schoolCrossover = new SchoolyardTeamsTSPCrossover();
-            
-            long startTimeA = System.nanoTime();
-            alg.evolutionaryAlgorithm(population, fitness, mutator, meanCrossover);
-            long endTimeA = System.nanoTime();
-            
-            System.out.println("FastNova run time: " + (endTimeA - startTimeA));
-            
-            long startTimeB = System.nanoTime();
-            algB.evolutionaryAlgorithm(populationB, fitness, superMutator, meanCrossover);
-            long endTimeB = System.nanoTime();
-            
-            System.out.println("SuperNova run time: " + (endTimeB - startTimeB));
-
-            long startTimeC = System.nanoTime();
-            algC.evolutionaryAlgorithm(populationC, fitness, offsetMutator, schoolCrossover);
-            long endTimeC = System.nanoTime();
-            
-            System.out.println("OffsetMutation run time: " + (endTimeC - startTimeC));
-            
+            EvolutionaryAlgorithm2 ea2 = new EvolutionaryAlgorithm2();
+            ea2.EvolutionaryAlgorithm(tsp);
             return;
+            
+            // TSPEvolutionaryAlgorithm alg = new TSPEvolutionaryAlgorithm(0.05, 4, 1000);
+            // TSPEvolutionaryAlgorithm algB = new TSPEvolutionaryAlgorithm(0.05, 4, 1000);
+            // TSPEvolutionaryAlgorithm algC = new TSPEvolutionaryAlgorithm(0.05, 4, 1000);
+            // ArrayList<TSP_Instance> population = new ArrayList<TSP_Instance>();
+            // ArrayList<TSP_Instance> populationB = new ArrayList<TSP_Instance>();
+            // ArrayList<TSP_Instance> populationC = new ArrayList<TSP_Instance>();
+            // int population_size = 10;
+            // int instance_size = 100;
+            // for (int i = 0; i < population_size; i++)
+            // {
+            //     population.add(new TSP_Instance(instance_size, 0, 100));
+            //     populationB.add(new TSP_Instance(instance_size, 0, 100));
+            //     populationC.add(new TSP_Instance(instance_size, 0, 100));
+            // }
+            
+            // TwoOptFitnessFunction fitness = new TwoOptFitnessFunction();
+            // FastNovaMutation mutator = new FastNovaMutation(3);
+            // SuperNovaMutation superMutator = new SuperNovaMutation(0, 0, 100, 100);
+            // OffsetMutation offsetMutator = new OffsetMutation();
+            // MeanTSPCrossover meanCrossover = new MeanTSPCrossover();
+            // SchoolyardTeamsTSPCrossover schoolCrossover = new SchoolyardTeamsTSPCrossover();
+            
+            // long startTimeA = System.nanoTime();
+            // alg.evolutionaryAlgorithm(population, fitness, mutator, meanCrossover);
+            // long endTimeA = System.nanoTime();
+            
+            // System.out.println("FastNova run time: " + (endTimeA - startTimeA));
+            
+            // long startTimeB = System.nanoTime();
+            // algB.evolutionaryAlgorithm(populationB, fitness, superMutator, meanCrossover);
+            // long endTimeB = System.nanoTime();
+            
+            // System.out.println("SuperNova run time: " + (endTimeB - startTimeB));
+
+            // long startTimeC = System.nanoTime();
+            // algC.evolutionaryAlgorithm(populationC, fitness, offsetMutator, schoolCrossover);
+            // long endTimeC = System.nanoTime();
+            
+            // System.out.println("OffsetMutation run time: " + (endTimeC - startTimeC));
+            
+            // return;
         }
     }
 }
